d008ee9a2027c4ec8fd89c8afc52d996ec84d44f
commanderaipa <gundamgundam777@gmail.com>
2022-05-02 01:05:19 +0900

    [add] pixelaモジュールの動作確認完了.
    ただ脆弱性があるっぽくてそれは要確認...

--- ganbari-graph/src/functions/hello/handler.ts
+++ ganbari-graph/src/functions/hello/handler.ts
@@ -7,9 +7,8 @@ import schema from './schema';
 
 const hello: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
   const graphId: string = "test-graph";
-  console.log(process.env.HOGE);
-  // const client = new Pixela(process.env.PIXELA_USER_NAME, process.env.PIXELA_USER_TOKEN);
-  // console.log(await client.incrementPixel(graphId));
+  const client = new Pixela(process.env.PIXELA_USER_NAME, process.env.PIXELA_USER_TOKEN);
+  console.log(await client.incrementPixel(graphId));
 
   return formatJSONResponse({
     message: `Hello ${event.body.name}, welcome to the exciting Serverless world!`,
